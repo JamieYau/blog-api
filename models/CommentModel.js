8873c4084a016465b6462801c9d516c12a5cdf05
JamieYau <jamie.yau2002@me.com>
2024-03-29 21:16:41 +0000

    Add models for blog data

---
+++ models/CommentModel.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const commentSchema = new Schema(
+  {
+    content: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    post: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Post",
+      required: true,
+    },
+  },
+);
+
+module.exports = mongoose.model("Comment", commentSchema);
