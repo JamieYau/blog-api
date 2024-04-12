4af67c5af73c8cee4e78c8c1f77f09caaceffaa2
JamieYau <jamie.yau2002@me.com>
2024-04-12 21:52:22 +0100

    Add JWT creation and authentication

--- app.js
+++ app.js
@@ -5,16 +5,19 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
+const { DB_URI } = process.env;
+
 const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
-const postsRouter = require("./routes/posts")
-const commentsRouter = require("./routes/comments")
+const postsRouter = require("./routes/posts");
+const commentsRouter = require("./routes/comments");
+const authRouter = require("./routes/auth");
 
 const app = express();
 
 // connect to database
 mongoose
-  .connect(process.env.DB_URI)
+  .connect(DB_URI)
   .then(() => console.log("Database connected successfully"))
   .catch((err) => console.log(err));
 
@@ -29,9 +32,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/posts", postsRouter)
-app.use("/comments", commentsRouter)
+app.use("/api/users", usersRouter);
+app.use("/api/posts", postsRouter);
+app.use("/api/comments", commentsRouter);
+app.use("/api/auth", authRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
---
+++ controllers/authController.js
@@ -0,0 +1,58 @@
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/UserModel");
+const asyncHandler = require("express-async-handler");
+const { JWT_SECRET } = process.env;
+
+// User login
+const login = asyncHandler(async (req, res) => {
+  const { username, password } = req.body;
+
+  // Find the user by username
+  const user = await User.findOne({ username });
+
+  // Check if user exists and if the password matches
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Invalid username or password" });
+  }
+
+  // Generate JWT token
+  const token = jwt.sign(
+    { userId: user._id, username: user.username },
+    JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+
+  res.status(200).json({ success: true, token });
+});
+
+// Middleware function to authenticate JWT token
+const authenticateToken = (req, res, next) => {
+  // Get the token from the Authorization header
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (!token) {
+    // If token is not provided, return 401 Unauthorized
+    return res.sendStatus(401);
+  }
+
+  // Verify the token
+  jwt.verify(token, JWT_SECRET, (err, user) => {
+    if (err) {
+      // If token is invalid, return 403 Forbidden
+      return res.sendStatus(403);
+    }
+
+    // Attach the authenticated user to the request object
+    req.user = user;
+    next(); // Call the next middleware
+  });
+};
+
+module.exports = {
+  login,
+  authenticateToken,
+};
