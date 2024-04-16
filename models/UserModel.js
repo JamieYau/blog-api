501fcbcab9798d667e61b6fc20433ef5b2f5c657
JamieYau <jamie.yau2002@me.com>
2024-04-16 01:29:59 +0100

    Refactor password hashing into pre save function in user model

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
+const asyncHandler = require("express-async-handler");
 
 const userSchema = new Schema({
   username: {
@@ -11,6 +13,23 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+});
+
+// Middleware to hash password before saving
+userSchema.pre("save", async function (next) {
+  // Only hash the password if it's modified or new
+  if (!this.isModified("password")) {
+    return next();
+  }
+
+  try {
+    // Hash the password with bcrypt
+    const hashedPassword = await bcrypt.hash(this.password, 10);
+    this.password = hashedPassword;
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = mongoose.model("User", userSchema);
