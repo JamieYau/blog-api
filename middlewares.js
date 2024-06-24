f45872236f151d8e8bb3791e2b6006f902733df2
JamieYau <jamie.yau2002@me.com>
2024-06-24 20:35:58 +0100

    Add cms login route with isAdmin middleware check

--- middlewares.js
+++ middlewares.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const User = require("./models/UserModel");
 
 // Middleware function to handle validation errors
 const handleValidationErrors = (req, res, next) => {
@@ -16,7 +17,23 @@ const isAdminMiddleware = (req, res, next) => {
   next();
 };
 
+// Middleware to check if user is admin during login
+const isAdminLoginMiddleware = async (req, res, next) => {
+  const { username } = req.body;
+
+  // Find the user by username
+  const user = await User.findOne({ username });
+
+  // Check if user exists and is an admin
+  if (!user || !user.isAdmin) {
+    return res.status(403).json({ message: "Access denied." });
+  }
+
+  next();
+};
+
 module.exports = {
   handleValidationErrors,
   isAdminMiddleware,
+  isAdminLoginMiddleware
 };
